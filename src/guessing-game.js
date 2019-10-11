78330d73d827cfedda6e6239848a512c12f75475
Victoria-Butrym <victoria-butrym@tut.by>
2019-10-11 16:57:15 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min; //set the min value
+        this.max = max; //set the max value
     }
 
     guess() {
-
+        this.number = Math.ceil((this.max + this.min) / 2); //an attempt
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number; //change the max value to current number
     }
 
     greater() {
-
+        this.min = this.number; //change the min value to current number
     }
 }
 
